abcd8c039c9906ed283d7cb633cec5878e8f97a5
mateofay2099 <mateofay2099@gmail.com>
2024-08-03 17:20:40 +0200

    add main products

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,24 +1,45 @@
-import { getProducts } from 'app/services/shopify/products'
-import Image from 'next/image'
-import styles from './MainProducts.module.sass'
+import { getMainProducts } from "app/services/shopify/products";
+import Image from "next/image";
+import styles from "./MainProducts.module.sass";
+import Link from "next/link";
 
 export const MainProducts = async () => {
-  const products = await getProducts()
+  const products = await getMainProducts();
 
   return (
     <section className={styles.MainProducts}>
       <h3>✨ New products released!</h3>
       <div className={styles.MainProducts__grid}>
-        {products?.map((product) => {
-          const imageSrc = product.images[0].src;
-          return (
-            <article key={product.id}>
-              <p>{product.title}</p>
-              <Image src={imageSrc} fill alt={product.title} loading="eager" />
-            </article>
-          )
-        })}
+        {products?.map(
+          (product: {
+            id: string;
+            title: string;
+            handle: string;
+            images: {
+              src: string;
+            }[];
+          }) => {
+            const imageSrc = product.images[0].src;
+            return (
+              <Link
+                key={product.id}
+                href={`/product/${product.handle}?id=${product.id}`}
+                className={styles.MainProducts__link}
+              >
+                <article>
+                  <p>{product.title}</p>
+                  <Image
+                    src={imageSrc}
+                    fill
+                    alt={product.title}
+                    loading="eager"
+                  />
+                </article>
+              </Link>
+            );
+          }
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -1,11 +1,13 @@
-import { env } from "app/config/env"
+import { env } from "app/config/env";
 
 export const shopifyUrls = {
   products: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`
+    all: `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/products.json`,
+    mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/collections/478159798575/products.json`,
   },
   collections: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`,
-    'products': (id: string) => `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/${id}/products.json`
-  }
-}
+    all: `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/smart_collections.json`,
+    products: (id: string) =>
+      `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/collections/${id}/products.json`,
+  },
+};
