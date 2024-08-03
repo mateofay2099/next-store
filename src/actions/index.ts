c781d7e1fd488fc88505f027b14deb1bb08d65d4
mateofay2099 <mateofay2099@gmail.com>
2024-08-03 17:32:58 +0200

    add cart

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,11 @@
 "use server";
 import { GraphQLClientSingleton } from "app/graphql";
+import { createCartMutation } from "app/graphql/mutations/createCartMutation";
 import { createUserMutation } from "app/graphql/mutations/createUserMutation";
 import { createAccessToken } from "app/utils/auth/createAccessToken";
 import { redirect } from "next/navigation";
+import { validateAccessToken } from "app/utils/auth/validateAccessToken";
+import { cookies } from "next/headers";
 
 export const handleCreateUser = async (formData: FormData) => {
   const formDataObject = Object.fromEntries(formData);
@@ -36,4 +39,38 @@ export const handleLogin = async (formData: FormData) => {
     formDataObject.password as string
   );
   redirect("/store");
+};
+
+export const handleCreateCart = async (items: CartItem[]) => {
+  const cookiesStore = cookies();
+  const accessToken = cookiesStore.get("accessToken")?.value as string;
+
+  if (!accessToken) redirect("/login");
+
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient();
+  const customer = await validateAccessToken();
+  const variables = {
+    input: {
+      buyerIdentity: {
+        customerAccessToken: accessToken,
+        email: customer?.email,
+      },
+      lines: items.map((item) => ({
+        merchandiseId: item.merchandiseId,
+        quantity: item.quantity,
+      })),
+    },
+  };
+
+  const {
+    cartCreate,
+  }: {
+    cartCreate?: {
+      cart?: {
+        checkoutUrl: string;
+      };
+    };
+  } = await graphqlClient.request(createCartMutation, variables);
+
+  return cartCreate?.cart?.checkoutUrl;
 };
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import styles from "./Header.module.sass";
 import { validateAccessToken } from "app/utils/auth/validateAccessToken";
+import dynamic from "next/dynamic";
+
+const NoSSRShoppingCart = dynamic(() => import("../ShoppingCart"), {
+  ssr: false,
+});
 
 export const Header = async () => {
   const customer = await validateAccessToken();
 
   return (
-    <header>
+    <header className={styles.Header}>
       <nav>
         <ul className={styles.Header__list}>
           <li>
@@ -16,12 +21,15 @@ export const Header = async () => {
             <Link href="/store">Store</Link>
           </li>
         </ul>
+      </nav>
+      <div className={styles.Header__user}>
         {customer?.firstName ? (
           <p>Hello {customer.firstName}!</p>
         ) : (
           <Link href="/login">Login</Link>
         )}
-      </nav>
+        <NoSSRShoppingCart />
+      </div>
     </header>
   );
 };
