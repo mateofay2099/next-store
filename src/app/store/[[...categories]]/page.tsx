d33720890ab0fee3592f282f39256d18b3704174
mateofay2099 <mateofay2099@gmail.com>
2024-08-03 16:51:19 +0200

    Add store structure

---
+++ src/app/store/[[...categories]]/page.tsx
@@ -0,0 +1,11 @@
+interface CategoryProps {
+  params: {
+    categories: string[];
+  };
+  searchParams?: string;
+}
+
+export default function Category(props: CategoryProps) {
+  const { categories } = props.params;
+  return <h1>Dynamic categories: {categories}</h1>;
+}
